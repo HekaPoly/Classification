25c8cde5bbc173c35cd8283535f686f05be73b54
Jeremy Thai <jeremythai01@gmail.com>
2020-10-17 16:12:08 -0400

    +notebook

--- LSTMAE_open_dataset/LSTMAE.py
+++ LSTMAE_open_dataset/LSTMAE.py
@@ -23,13 +23,13 @@ def evaluate_AE_model(X):
     n_timesteps = X.shape[1] 
     n_features = X.shape[2]
    
-    create encoder part
+    #create encoder part
     encoder_decoder = Sequential()
     encoder_decoder.add(LSTM(128, activation='relu', input_shape=(n_timesteps, n_features), return_sequences=True))
     encoder_decoder.add(LSTM(64, activation='relu', input_shape=(n_timesteps, n_features), return_sequences=False))
     # Le nombre de features extrait depend en fait du nombre de neurones du dernier layer de l encodeur 
 
-    create decoder part
+    #create decoder part
     encoder_decoder.add(RepeatVector(n_timesteps))
     encoder_decoder.add(LSTM(64, activation='relu', return_sequences=True))
     encoder_decoder.add(LSTM(128, activation='relu', return_sequences=True))
