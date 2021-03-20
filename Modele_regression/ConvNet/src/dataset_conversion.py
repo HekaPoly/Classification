500fb534dd4bbde12f1184122576963784ec6518
olivierSaintCyr <saintcyr.olivier1@gmail.com>
2021-03-20 16:47:39 -0400

    ajout du dataset en npz et ajout du code pour convertir le dataset .mat en npz

--- Modele_regression/ConvNet/src/dataset_conversion.py
+++ Modele_regression/ConvNet/src/dataset_conversion.py
@@ -8,10 +8,11 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 N_PHASES = 3
+N_ANGLES = 18
 
 def display_angle_sequence(angle_data, sequence):
     for j in range(N_PHASES):
-        for angle in range(18):
+        for angle in range(N_ANGLES):
             for i in range(len(angle_data[N_PHASES * sequence + j])):
                 angle_values[angle].append(angle_data[N_PHASES * sequence + j][i][angle])
     
@@ -25,20 +26,22 @@ def generate_np_dataset(emg_data, angle_data):
     angle_array = []
     
     for sequence in range(int(len(emg_data)/N_PHASES)):
-        emg_sequence = []
-        angle_sequence = []
         for i in range(N_PHASES):
-            emg_sequence += emg_data[N_PHASES * sequence + i].tolist()
-            angle_sequence += angle_data[N_PHASES * sequence + i].tolist()
-        emg_array.append(emg_sequence)
-        angle_array.append(angle_sequence)
+            emg_phase = emg_data[N_PHASES * sequence + i]
+            angle_phase = angle_data[N_PHASES * sequence + i]
+            
+            if i > 0:
+                if emg_phase.shape[1] == N_ANGLES and phase_data.shape[1] == N_PHASES:
+                    emg_seq = np.concatenate((emg_phase, emg_seq), axis=0)
+                    angle_seq = np.concatenate((angle_phase, angle_seq), axis=0)
+            else:
+                emg_seq = emg_phase
+                angle_seq = angle_phase
 
-    return np.array(emg_array), np.array(angle_array)
+        emg_array.append(emg_seq)
+        angle_array.append(angle_seq)
 
-def saveas_npy(dataset : dict):
-    pass
-
-TOTAL_SEQUENCES = 512
+    return np.asarray(emg_array), np.asarray(angle_array)
 
 if __name__=="__main__":
     if os.name == 'nt': # Windows
@@ -47,29 +50,19 @@ if __name__=="__main__":
         filepath = "../../Acquisition/Data/Dataset_avec_angles_tester"
 
     data = loadmat(filepath + "/KIN_MUS_UJI.mat")
-    #graph electrodes
-    electrodes_values = []
-    # for electrode in range(len(data['EMG_KIN_v4']['EMG_data'][0][0][0])):
-    #     electrodes_values.append([])
-    #     for i in range(len(data['EMG_KIN_v4']['EMG_data'][0][0])):
-    #         electrodes_values[-1].append(data['EMG_KIN_v4']['EMG_data'][0][0][i][electrode]) 
     
-    # for electrode in electrodes_values:
-    #     plt.plot(electrode)
-    # plt.show()
-
-    angle_values = [[] for i in range(18)]
     angle_data = data['EMG_KIN_v4']['Kinematic_data'][0]
     emg_data = data['EMG_KIN_v4']['EMG_data'][0]
-    # phase_data = data['EMG_KIN_v4']['Phase'][0]
-
-    #display_angle_sequence(angle_data, 5)
     
     emg_data, angle_data = generate_np_dataset(emg_data, angle_data)
     print(emg_data.shape)
     print(angle_data.shape)
-    print(angle_data[0])
-    # X_data, Y_data = extract_features(filepath)
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     X_data, Y_data, stratify = Y_data,test_size=0.20, random_state=42
-    # ) [[asdf]]
+    with open(filepath + '/dataset_emg_angles.npz', 'wb') as file:
+        np.savez(file, emg_data=emg_data, angle_data=angle_data)
+
+    # Loading file exemple
+    with open(filepath + '/dataset_emg_angles.npz', 'rb') as file:
+        data = np.load(file, allow_pickle=True)
+        print(data.files)
+        print(data['emg_data'].shape)
+        print(data['angle_data'].shape)
